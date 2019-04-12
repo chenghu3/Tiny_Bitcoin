515731d4289f561dfe145e6d4a4eb3d0c6bf6155
Xia <jxia11@sp19-cs425-g10-02.cs.illinois.edu>
2019-04-11 19:03:54 -0500

    change ports

--- experiment.py
+++ experiment.py
@@ -7,13 +7,14 @@ hostname = socket.gethostname()
 addr = socket.gethostbyname(hostname)  
 
 # Open clients, on ports 9000+n
+os.system("rm *.log")
 num_clients = int(sys.argv[1])
 for i in range(num_clients):
     newpid = os.fork()
     if newpid == 0:
         port = 9000 + i
         logfile = hostname + "_" + str(port) + ".log" # logfile: IP_PORT.log
-        os.system("./client " + str(9000+i) + " > " + logfile)
+        os.system("./client " + str(9900+i) + " > " + logfile)
         break
     else:
         time.sleep(0.5)
